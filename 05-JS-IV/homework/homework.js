b3bac11c8666dbc5891f4d072f031c7cf6a82b12
caapuu-nico <nnicou.s@gmail.com>
2022-08-26 14:00:51 -0300

    homework.js IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -161,6 +161,12 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+producto.calcularPrecioDescuento = function(){
+  var descuento = producto.precio * producto.porcentajeDeDescuento;
+  var precioConDescuento = producto.precio - descuento;
+return precioConDescuento
+}
+return producto
 
 }
 
